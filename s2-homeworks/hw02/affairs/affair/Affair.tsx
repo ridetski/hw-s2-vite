24a6bbdcb1e4a947952e774511de1f4de0b2060d
Nikita Ridetski <ridetski.nikita@gmail.com>
2024-01-29 21:23:18 +0300

    HW2-done2

--- s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -5,7 +5,7 @@ import s2 from '../Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
     affair: AffairType
-    deleteAffairCallback: (_id: number) => AffairType[] // need to fix any
+    deleteAffairCallback: (_id: number) => void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
